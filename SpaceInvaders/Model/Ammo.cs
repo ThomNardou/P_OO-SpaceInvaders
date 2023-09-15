642c2c7657369df1c121471d2e8ea355186f2bca
ReactedTitan880 <thomas.nardou@eduvaud.ch>
2023-09-15 10:36:59 +0200

    refactor(P-OO spaceInvaders) change the structure of the game

---
+++ SpaceInvaders/Model/Ammo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Ammo
+    {
+
+        public int xPos;
+        public int yPos;
+
+        ConsoleColor color;
+
+        private string[] view =
+        {
+            " O ",
+            " | "
+        }; 
+
+        public Ammo(int xPos, int yPos, ConsoleColor color)
+        {
+            this.xPos = xPos;
+            this.yPos = yPos;
+            this.color = color;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(xPos, yPos);
+            for (int i = 0; i < view.Length; i++)
+            {
+                Console.SetCursorPosition(xPos, yPos + i);
+                Console.WriteLine(view[i]);
+            }
+        }
+
+        public void Update()
+        {
+            yPos-=2;
+        }
+    }
+}
---
+++ SpaceInvaders/Model/Ennemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Ennemy
+    {
+
+        public int xPos;
+        public int yPos;
+
+        public bool goingLeft = false;
+
+        ConsoleColor color;
+
+        private string[] _enemy = { "{@v@}", "/\" \"\\" };
+
+        public Ennemy (int x, int y, ConsoleColor color)
+        {
+            this.xPos = x;
+            this.yPos = y;
+            this.color = color;
+        }
+
+
+        public void show()
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(xPos, yPos);
+            for (int i = 0; i < _enemy.Length; i++)
+            {
+                Console.SetCursorPosition(xPos, yPos + i);
+                Console.WriteLine(_enemy[i]);
+            }
+        }
+
+        public void updateEnnemyX()
+        {
+            if (goingLeft)
+            {
+                this.xPos-=3;
+            }
+            else
+            {
+                this.xPos+=3;
+            }
+        }
+
+        public void updateEnnemyY()
+        {
+            this.yPos += 2;
+        }
+    }
+}
